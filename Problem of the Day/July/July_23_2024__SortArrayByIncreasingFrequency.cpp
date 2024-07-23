4244871a97e4870102dbbd35bd43bed0d5dec90c
Anurag Panda <anuragpanda2704@gmail.com>
2024-07-23 21:27:09 +0530

    2 POTDs solved

---
+++ Problem of the Day/July/July_23_2024__SortArrayByIncreasingFrequency.cpp
@@ -0,0 +1,18 @@
+// Level: EASY
+
+class Solution {
+    public:
+        vector<int> frequencySort(vector<int>& nums) {
+            unordered_map<int,int>mp;
+            for(int i : nums)
+                mp[i]++;
+
+            sort(nums.begin(), nums.end(), [&](int n1, int n2){
+                if(mp[n1] != mp[n2])
+                    return mp[n1]<mp[n2];
+                else return n2<n1;
+            });
+            
+            return nums;
+        }
+};
