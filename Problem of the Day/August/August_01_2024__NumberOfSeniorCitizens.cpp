4fa399d12049a255b846ff953016d1232a5e6949
Anurag Panda <anuragpanda2704@gmail.com>
2024-08-01 20:30:09 +0530

    1 POTD solved

---
+++ Problem of the Day/August/August_01_2024__NumberOfSeniorCitizens.cpp
@@ -0,0 +1,15 @@
+// Level: EASY
+
+class Solution {
+    public:
+        int countSeniors(vector<string>& details) {
+            int c=0;
+            for(auto &i : details){
+                int age = stoi(i.substr(11,2));
+                if(age > 60)
+                    c += 1;
+            }
+
+            return c;
+        }
+};
