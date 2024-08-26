e07a0f2c60720b5c81d56a5a9183a11f620434e8
Anurag Panda <anuragpanda2704@gmail.com>
2024-08-26 10:19:32 +0530

    1 POTD solved

---
+++ Problem of the Day/August/August_26_2024__N-aryTreePostorderTraversal.cpp
@@ -0,0 +1,43 @@
+// Level: EASY
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    private:
+        vector<int> ans;
+
+        void traverse(Node* root) {
+            if(root==nullptr){
+                return;
+            }
+            for(auto c: root->children){
+                traverse(c);
+            }
+            ans.push_back(root->val);
+        }
+
+    public:
+            vector<int> postorder(Node* root) {
+                // Write the code here.
+                traverse(root);
+                return ans;
+            }
+};
