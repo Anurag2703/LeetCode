0cbce6309e25ce40852ec5f450fb543b57dd46b9
Anurag Panda <anuragpanda2704@gmail.com>
2025-04-30 09:37:25 +0530

    daily POTD solved

---
+++ Problem of the Day/2025/4. April/30_FindNumbersWithEvenNumberOfDigits.cpp
@@ -0,0 +1,11 @@
+// Level: EASY
+
+class Solution {
+    public:
+        int findNumbers(vector<int>& nums) {
+            int ans=0;
+            for(int x:nums)
+                ans+=int(floor(log10(x))+1)%2==0;
+            return ans;
+        }
+};
